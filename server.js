a6b1636f978cd61179fafff7333d1502eb128141
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2026-05-27 23:29:31 +0300

    .

--- server.js
+++ server.js
@@ -82,7 +82,14 @@ const io = new Server(server, {
   },
 });
 
-app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"] }));
+// CORS - allow all origins
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
 app.use(express.json());
 app.use("/uploads", express.static("./uploads"));
 
